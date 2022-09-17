4a0de62fab98713336e20d4d0d74dcf6dc2b1019
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-09-17 22:58:59 +0800

    add skeleton unit test

---
+++ tests/test_state_machine/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "greatest.h"
+
+GREATEST_MAIN_DEFS(); // important
+
+int main(int argc, char **argv) {
+    GREATEST_MAIN_BEGIN();
+
+
+
+    GREATEST_MAIN_END();
+    return 0;
+}
